92300126213a4da85a4641d9e527e44715260c1d
Luis <luise.ascencio13@gmail.com>
2023-10-06 19:08:16 -0600

    Primer ejercicio para la raspberry pi

---
+++ ejercicio1.py
@@ -0,0 +1,22 @@
+from gpiozero import LED, Button
+from time import sleep
+
+verde = LED(17)
+amarillo = LED(27)
+rojo = LED(22)
+boton = Button(2)
+
+"""
+verde.on()
+"""
+
+while True:
+    if boton.is_pressed:
+        rojo.off()
+        verde.on()
+    if boton.is_pressed:
+        verde.off()
+        amarillo.on()
+    if boton.is_pressed:
+        amarillo.off()
+        rojo.on()
