786b88103a3516923e7cb3478a16f2468bab4916
Luis <luise.ascencio13@gmail.com>
2023-10-06 20:16:17 -0600

    update

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,17 +6,13 @@ amarillo = LED(27)
 rojo = LED(22)
 boton = Button(2)
 
-"""
-verde.on()
-"""
-
 while True:
     if boton.is_pressed:
         rojo.off()
         verde.on()
-    if boton.is_pressed:
-        verde.off()
-        amarillo.on()
-    if boton.is_pressed:
-        amarillo.off()
-        rojo.on()
+        if boton.is_pressed:
+            verde.off()
+            amarillo.on()
+            if boton.is_pressed:
+                amarillo.off()
+                rojo.on()
