be9d1f01966baff092053b238525d03e109965c3
Luis <luise.ascencio13@gmail.com>
2023-10-06 20:35:22 -0600

    update ejercicio1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,18 +1,17 @@
 from gpiozero import LED, Button
 from time import sleep
 
-verde = LED(17)
-amarillo = LED(27)
-rojo = LED(22)
+l_verde = LED(17)
+l_amarillo = LED(27)
+l_rojo = LED(22)
 boton = Button(2)
+
+LEDs = [l_verde,l_amarillo,l_rojo]
+it = 2
 
 while True:
     if boton.is_pressed:
-        rojo.off()
-        verde.on()
-        if boton.is_pressed:
-            verde.off()
-            amarillo.on()
-            if boton.is_pressed:
-                amarillo.off()
-                rojo.on()
+        LEDs[it].off()          # Apagamos actual
+        it+=1                   # Siguiente led
+        it = i if i <3 else 0   #Reinicia desde 0 si es edge
+        LEDs[it].on()           #Prende el actual
